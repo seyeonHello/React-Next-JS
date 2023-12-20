64917655c869af2516bd782e9bc7f2420974a7a7
seyeon321 <seyeon321>
2023-12-20 18:09:27 +0900

    client-side rendering

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -7,21 +7,21 @@ import {Listbox, Transition} from "@headlessui/react";
 import {CustomFilterProps} from "@/types";
 import {updateSearchParams} from "@/utils";
 
-const CustomFilter = ({title, options}:CustomFilterProps) => {
+const CustomFilter = ({title, options, setFilter}:CustomFilterProps) => {
     const [selected, setSelected]=useState(options[0]);
-    const router=useRouter();
-    const handleUpdateParams=(e:{title:string, value:string})=>{
-        console.log(e)
-        const newPathName=updateSearchParams(title, e.value.toLowerCase())
-        router.push(newPathName)
-    }
+    //const router=useRouter();
+    // const handleUpdateParams=(e:{title:string, value:string})=>{
+    //     console.log(e)
+    //     const newPathName=updateSearchParams(title, e.value.toLowerCase())
+    //     router.push(newPathName)
+    // }
     return (
         <div className="w-fit">
             <Listbox
                 value={selected}
                 onChange={(e)=>{
                     setSelected(e)
-                    handleUpdateParams(e);
+                    setFilter(e.value);
                 }}
             >
                 <div className="relative w-fit z-10">
--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -4,12 +4,14 @@ import {useRouter} from "next/navigation";
 import {ShowMoreProps} from "@/types";
 import {CustomButton} from "@/components/index";
 import {updateSearchParams} from "@/utils";
-const ShowMore = ({pageNumber,isNext}:ShowMoreProps) => {
-    const router = useRouter();
+const ShowMore = ({pageNumber,isNext,setLimit}:ShowMoreProps) => {
+   // const router = useRouter();
     const handleNavigation = () => {
         const newLimit=(pageNumber+1)*10;
-        const newPathName = updateSearchParams("limit",`${newLimit}`)
-        router.push(newPathName)
+        setLimit(newLimit);
+        // const newLimit=(pageNumber+1)*10;
+        // const newPathName = updateSearchParams("limit",`${newLimit}`)
+        // router.push(newPathName)
     }
     return (
         <div className="w-full flex-center gap-5 mt-10">
